d7b2a505d6d55c574a6c03239a1e54162f99b68d
Kumar <prem.kumar-ext3@ab-inbev.com>
2026-07-12 16:45:31 +0530

    third commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello kumar prem from Azure!"
+    return "Hello kumar prem from Azure!  for testing purpose only"
 
 if __name__ == "__main__":
     app.run()
